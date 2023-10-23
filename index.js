72502bc24d31d146fe25a3239bf4e24fdc22e82f
kieranwatt <kieranmwatt@gmail.com>
2023-10-22 23:14:24 -0400

    hopeidowell

--- index.js
+++ index.js
@@ -84,19 +84,42 @@ const titles = [
   "Little Women",
 ];
 
+
 //Array of authors and the book they wrote
 //"--- wrote --- in ---"
-
+books.forEach(books => {
+console.log(books.authorFirst , books.authorLast,"wrote",books.name,"in",books.publishDate)});
 //Sort books from oldest to most recent
-
+const orderedage = books.sort(function(book1,book2){
+  return book1.publishDate - book2.publishDate;
+});
+console.log(orderedage)
 //sort books alphabetically
-
+titles.sort();
+console.log(titles);
 //Find who wrote War and Peace
-
+const author = books.find(({ name }) => name === "War and Peace");
+console.log(author);
 //how many books were written before 1900?
-
+const oldbooks = books.filter((books) => books.publishDate < 1900);
+console.log(oldbooks);
 //was there at least one book published within the last 100 years?
-
+const currentyear= 2023
+const bookPublish = books.some((book) => book.publishDate >= currentyear-100)
+if (bookPublish) {
+  console.log("Yes")
+}
+else {
+  console.log("no")
+}
 //was every book published within the last 100 years?
-
+const bookPublishAll = books.every((book) => book.publishDate >= currentyear-100)
+if (bookPublishAll) {
+  console.log("Yes")
+}
+else {
+  console.log("no")
+}
 //print a list of books that "includes" the genre historical
+const history = books.filter((type) => type.genre.includes ("historical"));
+console.log(history);
